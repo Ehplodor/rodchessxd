dd2e83cc3df42d59e502e2b55c25acfdc4421864
Ehplodor <80175881+Ehplodor@users.noreply.github.com>
2026-09-13 09:36:25 +0200

    build(web): mise à jour du cache_version du service worker

--- RodChessXD2.service.worker.js
+++ RodChessXD2.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789043762|3045790';
+const CACHE_VERSION = '1789284964|3873872';
 /** @type {string} */
 const CACHE_PREFIX = 'RodChessXD-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
